64b410a5507ce54c3e2b6f8c88843421e3f59323
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-06-15 18:14:22 +0500

    Update background colors and blur effects on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,14 @@ const Home = () => {
     <section className='container mx-auto my-10'>
       <div className='flex md:flex-row flex-col items-center gap-4 justify-between'>
         <div className='relative border h-[400px] z-[100] w-full md:w-[40%] bg_dog rounded-[30px] border-primary lg:p-10 p-5'>
-          <div className="bg-[#531b6b] z-[-10] h-[100%] filter_blur rounded-[10px] w-[100%] left-[20%] absolute" />
+          <div className="bg-[#772898] z-[-10] h-[100%] filter_blur rounded-[10px] w-[100%] left-[20%] absolute" />
           <h1 className='font-bungee  text-3xl'>DOG OF THE DAY</h1>
           <div className='md:w-[90%] w-full m-auto bg_header h-[250px] bg_black_gradient mt-5 rounded-[30px]'>
 
           </div>
         </div>
         <div className='relative border h-[400px] z-[100] w-full md:w-[55%] rounded-[30px] border-primary bg_glass lg:p-10 p-5 '>
-          <div className="bg-[#7e2d6a] h-[100%] z-[-10] filter_blur rounded-[10px] w-[80%] absolute" />
+          <div className="bg-[#c140a1] h-[100%] z-[-10] filter_blur rounded-[10px] w-[50%] left-[20%] absolute" />
           <h1 className='font-bungee text-3xl'>Token Matcher</h1>
         </div>
       </div>
