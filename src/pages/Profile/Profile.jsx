768b82710a5bf0eecb6de796ffb19267c95e7f5b
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-06-19 08:38:01 +0500

    structure for Profile page

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+    return (
+        <section className='container relative mx-auto lg:my-24 my-16 lg-px-0 px-2'>
+            <div className="bg_glass bg_conic border z-[100] h-[100vh] w-full md:rounded-[40px] rounded-[30px] border-primary lg:p-10 p-5">
+                <h1 className='font-bungee md:text-3xl text-xl'>Your Profile</h1>
+                <div className='w-[50%]'>
+                    <div className='bg_glass2 h-[50vh]'>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default Profile
